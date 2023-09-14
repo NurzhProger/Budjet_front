c20583a093e2349735e056bdd118c8c16f74ef25
Dulat1993 <140396468+Dulat1993@users.noreply.github.com>
2023-09-14 14:41:40 +0500

    140920231441

--- src/app/documents/Limit/limit-element/limit-element.component.ts
+++ src/app/documents/Limit/limit-element/limit-element.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-limit-element',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./limit-element.component.css']
 })
 export class LimitElementComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() limit_id = ''
+  constructor() {
+    this.items = [
+      {
+        label: 'Записать',
+        icon: 'pi pi-save',
+        command: () => {
+          this.saveDoc(false);
+        }
+      }
+    ]
+  }
+  items: MenuItem[];
+  form: FormGroup
   ngOnInit(): void {
+  }
+
+  saveDoc(close: boolean): void {
+    let responce: any
+
+    // this.utvDetailService.saveUtv(this.utvDetail)
+    //   .subscribe(
+    //     (data) => (
+    //       this.utvDetailmsg.add({ severity: 'success', summary: 'Успешно', detail: 'Документ успешно записан!' }),
+    //       responce = data, this.utvDetail = responce, this.closeaftersave(close)
+    //     ),
+    //     (error) => (
+    //       this.utvDetailmsg.add({ severity: 'error', summary: 'Ошибка', detail: error.error.status })
+    //     )
+    //   )
+  }
+
+  closeform(close: boolean) {
+
+  //   let objString = JSON.stringify(this.utvDetail)
+  //   this.hashEnd = SHA256(objString).toString()
+
+  //   if (close) {
+  //     if (this.hashBegin == this.hashEnd) {
+  //       this.closeEvent.emit()
+  //     }
+  //     else {
+  //       this.utvDetailconfirm.confirm({
+  //         message: 'Данные были изменены. Закрыть документ?',
+  //         header: 'Закрытие',
+  //         icon: 'pi pi-exclamation-triangle',
+  //         accept: () => {
+  //           this.closeEvent.emit()
+  //           this.utvDetailconfirm.close()
+  //         },
+  //         reject: () => {
+  //           this.utvDetailconfirm.close()
+  //         }
+  //       })
+  //     }
+  //   }
   }
 
 }
