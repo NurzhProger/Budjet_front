1d5901002704c6eacc1eff3ccc2a6a5bbb97616e
NurzhProger <nuri.301193@gmail.com>
2023-09-25 16:21:12 +0500

    25092023_1621

--- src/app/documents/Budget_Ras4et/budget-ras4et-detail/budget-ras4et-detail.component.ts
+++ src/app/documents/Budget_Ras4et/budget-ras4et-detail/budget-ras4et-detail.component.ts
@@ -5,9 +5,27 @@ import { DialogService, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dy
 import { catchError, throwError, timeout } from 'rxjs'
 import { EnstruListComponent } from 'src/app/directory/planirovanie/ensTRU/enstru-list/enstru-list.component';
 import { ensTRU_element } from 'src/app/directory/planirovanie/ensTRU/interfaces';
+import { stazh_category_element } from 'src/app/directory/planirovanie/stazh-category/interfaces';
+import { category_sotr_element } from 'src/app/directory/planirovanie/category-sotr/interfaces';
+import { dolzhnost_element } from 'src/app/directory/planirovanie/dolzhnost/interfaces';
+import { podrazdelenie_element } from 'src/app/directory/podrazdelenie/interfaces';
+import { DoplNadbavkaElementComponent } from 'src/app/directory/planirovanie/dopl_nadbavka/dopl-nadbavka-element/dopl-nadbavka-element.component';
+import { OblastiRegionyElementComponent } from 'src/app/directory/planirovanie/oblasti-regiony/oblasti-regiony-element/oblasti-regiony-element.component';
+import { marki_avto_element } from 'src/app/directory/planirovanie/marki_avto/interfaces';
+import { ed_izm_element } from 'src/app/directory/planirovanie/ed-izm/interfaces';
 import { Ras4et_doc, ChildItem, TableItemPass } from "../Budget_ras4et.interfaces";
 import { budjetRas4et_Service } from "../Budget_ras4et.Services";
 import { SHA256 } from 'crypto-js';
+import { EdIzmListComponent } from 'src/app/directory/planirovanie/ed-izm/ed-izm-list/ed-izm-list.component';
+import { CategorySotrListComponent } from 'src/app/directory/planirovanie/category-sotr/category-sotr-list/category-sotr-list.component';
+import { DolznostListComponent } from 'src/app/directory/planirovanie/dolzhnost/dolznost-list/dolznost-list.component';
+import { PodrazdelenieListComponent } from 'src/app/directory/podrazdelenie/podrazdelenie-list/podrazdelenie-list.component';
+import { doplaty_nadbavky_element } from 'src/app/directory/planirovanie/dopl_nadbavka/interfaces';
+import { DoplNadbavkaListComponent } from 'src/app/directory/planirovanie/dopl_nadbavka/dopl-nadbavka-list/dopl-nadbavka-list.component';
+import { oblasti_element } from 'src/app/directory/planirovanie/oblasti-regiony/interfaces';
+import { OblastiRegionyListComponent } from 'src/app/directory/planirovanie/oblasti-regiony/oblasti-regiony-list/oblasti-regiony-list.component';
+import { MarkiAvtoListComponent } from 'src/app/directory/planirovanie/marki_avto/marki-avto-list/marki-avto-list.component';
+import { StazhCategoryListComponent } from 'src/app/directory/planirovanie/stazh-category/stazh-category-list/stazh-category-list.component';
 
 @Component({
   selector: 'app-budget-ras4et-detail',
@@ -94,6 +112,155 @@ export class BudgetRas4etDetailComponent implements OnInit {
     })
   }
 
+  selectEd_Izm(ed_izm: ed_izm_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(EdIzmListComponent,
+      {
+        header: 'Выбор единицы измерения',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((ed_izm_element: ed_izm_element) => {
+      if (ed_izm_element) {
+        ed_izm.name = ed_izm_element.name
+        ed_izm.id = ed_izm_element.id
+      }
+    })
+  }
+
+  selectCategorySotr(cat_sotr: category_sotr_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(CategorySotrListComponent,
+      {
+        header: 'Выбор категории',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((category_sotr_element: category_sotr_element) => {
+      if (category_sotr_element) {
+        cat_sotr = category_sotr_element
+      }
+    })
+  }
+
+  selectDolzhnost(dolzh_el: dolzhnost_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(DolznostListComponent,
+      {
+        header: 'Выбор должности',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((dolzhnost_element: dolzhnost_element) => {
+      if (dolzhnost_element) {
+        dolzh_el = dolzhnost_element
+      }
+    })
+  }
+
+  selectPodrazdelenie(podr_el: podrazdelenie_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(PodrazdelenieListComponent,
+      {
+        header: 'Выбор подразделении',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((podrazdelenie_element: podrazdelenie_element) => {
+      if (podrazdelenie_element) {
+        podr_el = podrazdelenie_element
+      }
+    })
+  }
+
+  selectDoplNadb(dopl_nad: doplaty_nadbavky_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(DoplNadbavkaListComponent,
+      {
+        header: 'Выбор доплаты и надбавки',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((doplaty_nadbavky_element: doplaty_nadbavky_element) => {
+      if (doplaty_nadbavky_element) {
+        dopl_nad = doplaty_nadbavky_element
+      }
+    })
+  }
+
+  selectOblreg(obl_reg: oblasti_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(OblastiRegionyListComponent,
+      {
+        header: 'Выбор области регионов',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((oblasti_element: oblasti_element) => {
+      if (oblasti_element) {
+        obl_reg = oblasti_element
+      }
+    })
+  }
+
+  selectMarkiAvto(marki: marki_avto_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(MarkiAvtoListComponent,
+      {
+        header: 'Выбор автомобиля',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((marki_avto_element: marki_avto_element) => {
+      if (marki_avto_element) {
+        marki = marki_avto_element
+      }
+    })
+  }
+
+  selectStazh(stazh_cat: stazh_category_element) {
+
+    this.Budget_ras4et_Detailref = this.Budget_ras4et_DialogService.open(StazhCategoryListComponent,
+      {
+        header: 'Выбор стажа',
+        width: 'calc(60%)',
+        height: 'calc(80%)'
+      })
+    this.Budget_ras4et_Detailref.onClose.subscribe((stazh_category_element: stazh_category_element) => {
+      if (stazh_category_element) {
+        stazh_cat = stazh_category_element
+      }
+    })
+  }
+
+  onInputChange(value: string, kolon: ChildItem, ri: number) {
+    let str_num = kolon.stroka;
+    let filtered_tbl= [this.Ras4et_detail.tbl[ri]]
+    let aaa = '1234567890'
+    for (let i = 0; i <= filtered_tbl.length; i++) {
+      console.log(filtered_tbl[i])
+      if (filtered_tbl[i].columns_used !== '') {
+        console.log(
+          
+          'asdadsasdasdsda'
+        )
+        // let mass_simv = filtered_tbl[i].columns_used.split(' ')
+        // console.log(mass_simv)
+        // for (let y = 0; y < mass_simv.length; y++) {
+        //   console.log(mass_simv[y]);
+          
+        //     // let proverka = aaa.search(y[0])
+        // }
+      }
+      console.log(filtered_tbl[i]);
+      
+    }
+    // console.log(this.Ras4et_detail.tbl[ri]);
+    // console.log(str_num);
+    
+    
+    // Дополнительная обработка здесь
+  }
 
   conculate() {
     // for (var stroka1 of this.Ras4et_detail.tbl) {
