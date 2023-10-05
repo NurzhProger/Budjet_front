a5c46e6c0b6b7ed71edbae32e5594b024a89ac34
Dulat1993 <140396468+Dulat1993@users.noreply.github.com>
2023-10-05 09:16:01 +0500

    051020230915

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/taimas'),
+      dir: require('path').join(__dirname, './coverage/budjet'),
       subdir: '.',
       reporters: [
         { type: 'html' },
