754da9a3d6fd2af60ffc1d4d981339ccc610727b
MagzhanJSoft <magzhanatullaev@gmail.com>
2023-10-18 14:57:48 +0500

    20231018_1457

--- src/app/directory/planirovanie/stazh-category/interfaces.ts
+++ src/app/directory/planirovanie/stazh-category/interfaces.ts
@@ -11,9 +11,3 @@ export interface stazh_category_list {
     previous?: string,
     results: [stazh_category_element]
 }
-export interface stazh_category_select {
-    count?: number,
-    next: string,
-    previous?: string,
-    results: [stazh_category_element]
-}
--- src/app/directory/planirovanie/stazh-category/stazh-category-select/stazh-category-select.component.ts
+++ src/app/directory/planirovanie/stazh-category/stazh-category-select/stazh-category-select.component.ts
@@ -1,16 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Observable } from 'rxjs';
-import { stazh_category_element, stazh_category_list, stazh_category_select } from '../interfaces';
+import { stazh_category_element, stazh_category_list } from '../interfaces';
 import { StazhCategoryElementComponent } from '../stazh-category-element/stazh-category-element.component';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { StazhCategoryService } from '../stazh-category.service';
 import { MessageService } from 'primeng/api';
 
 @Component({
-    selector: 'app-stazh-category-select',
-    templateUrl: './stazh-category-select.component.html',
-    styleUrls: ['./stazh-category-select.component.css']
-  })
+  selector: 'app-stazh-category-select',
+  templateUrl: './stazh-category-select.component.html',
+  styleUrls: ['./stazh-category-select.component.css']
+})
 export class StazhCategorySelectComponent implements OnInit {
   constructor(
     private stazhCategoryService: StazhCategoryService,
@@ -19,64 +19,64 @@ export class StazhCategorySelectComponent implements OnInit {
     private stazh_category_message_servis: MessageService
   ) { }
 
-@Output() closeEvent = new EventEmitter<any>()
-@Input() data = false
-stazh_categorys$: Observable<stazh_category_select>
-windowHeight: number
-selected: any
-first = 0
-rows = 25  
-searchSta: ''
-stazh: any
+  @Output() closeEvent = new EventEmitter<any>()
+  @Input() data = false
+  stazh_categorys$: Observable<stazh_category_list>
+  windowHeight: number
+  selected: any
+  first = 0
+  rows = 25
+  searchSta = ''
+  stazh: any
 
-ngOnInit(): void {
-  this.fetchSta(),
-  this.updateWindowSize() 
-}
-
-private updateWindowSize() {
-  this.windowHeight = window.innerHeight;
-}
-
-fetchSta() {
-  let params = {
-    limit: this.rows.toString(),
-    offset: this.first.toString(),
-    searchSta: this.searchSta
-  }
-  this.stazh_categorys$ = this.stazhCategoryService.fetch(params);
-}
-
-onSelected(stazh:stazh_category_element) {
-  if (!this.selected) {
-  this.stazh_category_message_servis.add({ severity: 'error', summary: 'Ошибка', detail: 'Выберите стажа!' })
-  return
-  }
-  this.stazh_category_dialog_ref.close(stazh)
+  ngOnInit(): void {
+    this.fetchSta(),
+      this.updateWindowSize()
   }
 
-onRowClick(stazh: stazh_category_element) {
-  this.stazh_category_dialog_ref.close(stazh)
+  private updateWindowSize() {
+    this.windowHeight = window.innerHeight;
   }
 
-onRowEdit(stazh: stazh_category_element) {
-  this.stazh_category_dialog_ref = this.stazh_category_dialog_servis.open(StazhCategoryElementComponent,
-    {
-      header: 'Редактирование стажа категории',
-      width: '60%',
-      height: '60%',
-      data: { stazh_id: stazh.id }
-    })
-  this.stazh_category_dialog_ref.onClose.subscribe((save: boolean) => {
-    if (save) {
-    this.fetchSta()
+  fetchSta() {
+    let params = {
+      limit: this.rows.toString(),
+      offset: this.first.toString(),
+      searchSta: this.searchSta
     }
-  })
-}
+    this.stazh_categorys$ = this.stazhCategoryService.fetch(params);
+  }
 
-onPageChange(event: any) {
-  this.first = event.first
-  this.rows = event.rows
-  this.fetchSta()
-}
+  onSelected(stazh: stazh_category_element) {
+    if (!this.selected) {
+      this.stazh_category_message_servis.add({ severity: 'error', summary: 'Ошибка', detail: 'Выберите стажа!' })
+      return
+    }
+    this.stazh_category_dialog_ref.close(stazh)
+  }
+
+  onRowClick(stazh: stazh_category_element) {
+    this.stazh_category_dialog_ref.close(stazh)
+  }
+
+  onRowEdit(stazh: stazh_category_element) {
+    this.stazh_category_dialog_ref = this.stazh_category_dialog_servis.open(StazhCategoryElementComponent,
+      {
+        header: 'Редактирование стажа категории',
+        width: '60%',
+        height: '60%',
+        data: { stazh_id: stazh.id }
+      })
+    this.stazh_category_dialog_ref.onClose.subscribe((save: boolean) => {
+      if (save) {
+        this.fetchSta()
+      }
+    })
+  }
+
+  onPageChange(event: any) {
+    this.first = event.first
+    this.rows = event.rows
+    this.fetchSta()
+  }
 }
