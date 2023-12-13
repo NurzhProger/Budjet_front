12023a3ed76e65cfea586d3dd7a7ab71cd1691dc
BlackRose98 <146694410+BlackRose98@users.noreply.github.com>
2023-12-13 14:39:07 +0300

    13122023_1439

--- src/app/documents/budget_income/budget-income-detail/budget-income-detail.component.ts
+++ src/app/documents/budget_income/budget-income-detail/budget-income-detail.component.ts
@@ -8,6 +8,8 @@ import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { OrganizationDetailComponent } from 'src/app/directory/organization/organization-detail/organization-detail.component';
 import { OrganizationSelectComponent } from 'src/app/directory/organization/organization-select/organization-select.component';
 import { organization_detail } from 'src/app/directory/organization/interfaces';
+import { SpecificationIncomeSelectComponent } from 'src/app/directory/income/specification-income/specification-income-select/specification-income-select.component';
+import { specification_income_detail } from 'src/app/directory/income/specification-income/interfaces';
 
 @Component({
   selector: 'app-budget-income-detail',
@@ -164,5 +166,60 @@ export class BudgetIncomeDetailComponent implements OnInit {
   changeGodUch() {
     this.budget_income_detail.head.god_ucheta = String(this.godNumber + '-01-01')
   }
+
+  openNew() {
+    this.budgetinc_Rer = this.budgetIncDialog.open(SpecificationIncomeSelectComponent,
+      {
+        header: 'Выбор специфика поступления',
+        width: '60%',
+        height: '80%',
+        data: { _org_id: this.budget_income_detail.head._organization.id }
+      })
+
+    this.budgetinc_Rer.onClose.subscribe((spec: specification_income_detail) => {
+      if (spec) {
+        this.budget_income_detail.tbl.push(
+          {
+            _spec_income: {
+              id: spec.id,
+              code: spec.code,
+              name_kaz: spec.name_kaz,
+              name_rus: spec.name_rus
+            },
+            id: 0,
+            summ: 0,
+            _budjet_income: {
+              id: 0,
+              nom: '',
+              org_name: ''
+            }
+          })
+      }
+    })
+  }
+
+  onDelete(_spec_income_id: number, _spec_income_name: string) {
+
+    this.budgetincConfirmation.confirm({
+      message: 'Вы действительно хотите удалить?',
+      header: 'Удаление',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        for (let i = this.budget_income_detail.tbl.length - 1; i >= 0; i--) {
+          let index = this.budget_income_detail.tbl.findIndex(item => _spec_income_id === item._spec_income.id)
+          if (index !== -1) {
+            this.budget_income_detail.tbl.splice(index, 1)
+          }
+        }
+        this.budgetincConfirmation.close()
+      },
+      reject: () => {
+        this.budgetincConfirmation.close();
+      }
+    });
+  }
+
+  
+
 
 }
